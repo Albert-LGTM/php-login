0251ff76c138cc4341d58a5a52596aadb89a31f0
Albert-LGTM <albertdepalo@gmail.com>
2025-04-07 15:34:26 +0200

    better error logging

--- src/database.php
+++ src/database.php
@@ -1,12 +1,14 @@
 <?php
-$server = $_ENV['DB_SERVER'];
-$username = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASSWORD'];
-$database = $_ENV['DB_NAME'];
+$server = getenv('DB_SERVER');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
 
-try{
-	$conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$database;charset=utf8mb4", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
-	header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error');
-	die();
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error');
+    echo "Database connection failed: " . $e->getMessage();
+    die();
 }
